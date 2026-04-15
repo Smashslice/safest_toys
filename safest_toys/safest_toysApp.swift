d2e8da2266b9018a62952d3bb8e7d60032652958
Paul <smashslice@gmail.com>
2026-04-15 09:10:08 -0500

    Initial Commit

---
+++ safest_toys/safest_toysApp.swift
@@ -0,0 +1,17 @@
+//
+//  safest_toysApp.swift
+//  safest_toys
+//
+//  Created by BUTE, PAUL A. on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct safest_toysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
