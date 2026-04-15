356c643203242d67c721cfae2547b38ffb66de84
kn0960866 <norriskaydence@gmail.com>
2026-04-15 10:05:53 -0500

    Real About page

--- safest_toys/AboutView.swift
+++ safest_toys/AboutView.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 16) {
+                Image(systemName: "person.slash")
+                                    .font(.system(size: 90))
+                                    .foregroundStyle(.pink)
+                Text("Kaydence, Paul, Luke, Isaac")
+                    .font(.title)
+                Divider()
+                Text("An app that lets you pick the safest toys for your kids. These are proven to be safe and safe to use. ")
+                    .padding()
+            }
+            .padding()
+        }
+        .navigationTitle("About")
     }
-}
+    }
 
 #Preview {
     AboutView()
